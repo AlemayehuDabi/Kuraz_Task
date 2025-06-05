fd74a21e40d322b30a85e792b5e9811eead2cacb
Alemayehu <alemayehudabi606@gmail.com>
2025-06-05 14:19:22 +0300

    FEAT: connect to mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import { connectDB } from "./db";
 
 const app = express();
 const PORT = 3000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`running ${PORT}`);
